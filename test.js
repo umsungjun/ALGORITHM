a56cda31971d3de0ea5fe840c0e69cba4fa5ebd7
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-01-26 22:56:27 +0900

    Update test.js

--- test.js
+++ test.js
@@ -66,18 +66,97 @@
 // combination(input)
 // console.log(count);
 
-let input = [1, 2, 3, 4]
-let output = []
-let count = 0
+// let input = [1, 2, 3, 4]
+// let output = []
+// let count = 0
 
-function combination(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            console.log(arr[i], arr[j]);
-            count++
-        }
-    }
+// function combination(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             console.log(arr[i], arr[j]);
+//             count++
+//         }
+//     }
+// }
+
+// combination(input)
+// console.log(count);
+
+
+// let result
+// let count = 0
+// function forloop(s, t, number) {
+//     let acc = 0
+
+//     for (let i = 1; i <= number; i++) {
+//         if (i === 1) {
+//             acc += s
+//         } else {
+//             acc += t
+//         }
+//         console.log(i, acc)
+//     }
+//     return acc
+// }
+
+
+
+// function recusive(s, t, number) {
+//     // 멈출 조건
+//     if (number == 1) {
+//         return s
+//     }
+
+//     return recusive(s, t, number - 1) + t
+// }
+
+// result = recusive(3, 2, 5) // 초기값 3 등차값 2 반복횟수 5
+// console.log(result);
+
+
+// const double = (crr, db, num) => {
+//     for (let i = 1; i <= num; i++) {
+//         if (i == 1) {
+//             crr *= 1
+//         } else {
+//             crr *= db
+//         }
+//     }
+//     return crr
+// }
+
+// result = double(3, 2, 5)
+
+// console.log(result);
+
+// const double = (crr, db, num) => {
+//     if (num == 1) return crr
+
+//     return double(crr, db, num - 1) * 2
+// }
+
+// // 24 * 2
+// // 12 * 2
+// // 6 * 2
+// // 3 * 2
+
+
+// result = double(3, 2, 5)
+
+// console.log(result);
+
+const recusive = (crr) => {
+    if (crr == 1) return crr
+
+    return recusive(crr - 1) * crr
 }
 
-combination(input)
-console.log(count);
+// recusive(crr - 1) * crr
+// recusive(crr - 1) * crr
+// recusive(crr - 1) * crr
+// recusive(crr - 1) * crr
+
+
+
+result = recusive(5)
+console.log(result);
