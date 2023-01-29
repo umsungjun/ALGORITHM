b703540cee5f2490beedc01d57d65c44711642f1
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-01-29 22:34:15 +0900

    Update 기본문제풀이_등차수열의항찾기_문제.js

--- 수학 기본 이론/수학기본이론_07기본문제풀이_등차수열의항찾기/기본문제풀이_등차수열의항찾기_문제.js
+++ 수학 기본 이론/수학기본이론_07기본문제풀이_등차수열의항찾기/기본문제풀이_등차수열의항찾기_문제.js
@@ -4,13 +4,25 @@
 function answer(a, d, n) {
   let index = -1;
 
-  // 코드 구현 시작 영역
+  // let num
 
-  // …
+  // for (let i = 1; ; i++) { // i = 1부터 종료시점이 없이 무한 으로 i++
+  //   num = a + d * (i - 1) // 1, 3, 5, 7
 
-  // 코드 구현 종료 영역
+  //   if (num > n) break; // n보다 커지게 되면 break
 
-  return index;
+  //   if (num === n) {
+  //     index = i
+  //   }
+
+  // }
+
+  if ((n - a) % d === 0) {
+    // 등차수열을 만족함
+    index = (n - a) / d + 1
+  } else index = -1
+
+  return index
 }
 
 /* main code */
