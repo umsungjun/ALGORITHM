c97d90ff1089a78754d1852c796da3d4ee2cb0b7
umsungjun <umseongjun@naver.com>
2023-02-08 17:33:36 +0900

    Update 스택문제풀이_기차운행_문제.js

--- 선형 자료구조/선형자료구조_34스택문제풀이_기차운행/스택문제풀이_기차운행_문제.js
+++ 선형 자료구조/선형자료구조_34스택문제풀이_기차운행/스택문제풀이_기차운행_문제.js
@@ -1,11 +1,26 @@
 /* 기차 운행 */
+let stack =[]
+let num =0
 
 /* user code */
 function answer(train) {
-  // 코드 구현 시작 영역
-  // …
-  // 코드 구현 종료 영역
+  for(let i=0; i<train.length; i++){
+    while(stack.length ===0 || stack[stack.length -1] < train[i]){
+      stack.push(++num)
+    }
+    console.log(stack)
+    if(stack[stack.length-1] === train[i]){
+      stack.pop()
+    }else{
+      return false
+    }
+  }
+
+  return true
+  
+
 }
+
 
 /* main code */
 let input = [
