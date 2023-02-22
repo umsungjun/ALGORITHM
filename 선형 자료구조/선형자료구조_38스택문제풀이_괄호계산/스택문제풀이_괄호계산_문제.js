7e350faf0aa294140fb473330a0db19074a95ff4
umsungjun <umseongjun@naver.com>
2023-02-22 09:51:48 +0900

    Update 스택문제풀이_괄호계산_문제.js

--- 선형 자료구조/선형자료구조_38스택문제풀이_괄호계산/스택문제풀이_괄호계산_문제.js
+++ 선형 자료구조/선형자료구조_38스택문제풀이_괄호계산/스택문제풀이_괄호계산_문제.js
@@ -4,12 +4,56 @@
 function answer(str) {
   let result = 0;
 
-  // 코드 구현 시작 영역
+  // '('를 만날 경우에는 *2
+  // '['를 만날 경우에는 *3
+  // ')'를 만날 경우에는 /2
+  // '['를 만날 경우에는 /3
+  // '()', '[]' 만날경우에는 temp값을 result에 즉식 더해 줌
 
-  // …
+  let stack =[]
+  let temp = 1
+  // "(()[[]])"
 
-  // 코드 구현 종료 영역
+  // temp 2
+  // stack =[ (]
+  // result 4
+  for(let i=0; i<str.length; i++){
+    let mark = str[i]
 
+    switch(mark){
+      case '(':
+        temp *=2
+        stack.push(mark)
+        break
+      case '[':
+        temp *=3
+        stack.push(mark)
+        break
+      case ')':
+        if(stack.length ==0 || stack[stack.length-1] != '('){
+          return 0
+        } // 열린 괄호가 없는데 닫힌 과로가 들어올 경우
+        if(str[i-1] === '('){
+          result +=temp
+        }
+        stack.pop()
+        temp/=2
+        break
+      
+      case ']':
+        if(stack.length ===0 || stack[stack.length-1] != '['){
+          return 0
+        }
+
+        if(str[i-1] === '['){
+          result +=temp
+        }
+        stack.pop()
+        temp/=3
+        break
+    }
+  }
+  if(stack.length !==0) return 0
   return result;
 }
 
