b7962d76dfcd7f6acbb8e7074e237dbe829d227e
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-05-27 23:46:19 +0900

    Update test.js

--- test.js
+++ test.js
@@ -673,20 +673,20 @@ class LinkedList{
 } */
 
 //Stack() : 생성자 함수
-function Stack(array) {
-  this.array = array ? array : [];
-}
+// function Stack(array) {
+//   this.array = array ? array : [];
+// }
 
-// getBuffer() : 객체 내 데이터 셋 반환
-Stack.prototype.getBuffer = function () {
-  return this.array.slice();
-};
+// // getBuffer() : 객체 내 데이터 셋 반환
+// Stack.prototype.getBuffer = function () {
+//   return this.array.slice();
+// };
 
-// isEmpty() : 객체 낸 데이터 o / x
+// // isEmpty() : 객체 낸 데이터 o / x
 
-Stack.prototype.isEmpty = function () {
-  return this.array.length === 0;
-};
+// Stack.prototype.isEmpty = function () {
+//   return this.array.length === 0;
+// };
 
 // let stack = new Stack([1,2,3])
 // let data = stack.getBuffer()
